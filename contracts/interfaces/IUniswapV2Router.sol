1d434f86d6e723fb853ae2fb2857daa8d8c809b4
akshaynexus <akshaycm@hotmail.com>
2021-06-13 01:24:54 +0300

    fix: compile errors

--- contracts/interfaces/IUniswapV2Router.sol
+++ contracts/interfaces/IUniswapV2Router.sol
@@ -9,4 +9,14 @@ interface IUniswapV2Router {
         address to,
         uint256 deadline
     ) external returns (uint256[] memory amounts);
+
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function getAmountsOut(uint amountIn,address[] calldata path) external view returns (uint[] memory amounts);
 }
